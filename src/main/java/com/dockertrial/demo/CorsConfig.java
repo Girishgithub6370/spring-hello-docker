fe6bfbe3450519b1fda8724a3772fcb429fa0a61
Girishgithub6370 <kumargirish6370@gmail.com>
2025-08-28 09:14:51 +0530

    Enabled global CORS

---
+++ src/main/java/com/dockertrial/demo/CorsConfig.java
@@ -0,0 +1,22 @@
+package com.dockertrial.demo;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+@Configuration
+public class CorsConfig {
+    @Bean
+    public WebMvcConfigurer corsConfigurer() {
+        return new WebMvcConfigurer() {
+            @Override
+            public void addCorsMappings(CorsRegistry registry) {
+                registry.addMapping("/**")              // all endpoints
+                        .allowedOrigins("*")            // allow ALL origins
+                        .allowedMethods("*")            // allow ALL methods
+                        .allowedHeaders("*");           // allow ALL headers
+            }
+        };
+    }
+}
